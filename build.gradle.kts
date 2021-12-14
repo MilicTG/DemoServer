afd9decfad61d375caf738991837bd16b00072f3
MilicTG <milic.tg@gmail.com>
2021-12-14 10:02:33 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "com.delminiusdevs"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
